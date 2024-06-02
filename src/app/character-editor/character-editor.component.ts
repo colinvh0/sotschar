3bbbee2426fa0be3da2e4c191245337cf42a669e
colinvh0 <unkempt@gmail.com>
2024-06-01 19:21:23 -0700

    ...

--- src/app/character-editor/character-editor.component.ts
+++ src/app/character-editor/character-editor.component.ts
@@ -12,11 +12,13 @@ import { Tables } from '../character';
   styleUrl: './character-editor.component.less'
 })
 export class CharacterEditorComponent {
+  adjectives: {'key': string, 'value': string}[] = [];
+  allegiances: {'key': string, 'value': string}[] = [];
+  gear: {'key': string, 'value': string}[] = [];
   formGroup = new FormGroup({
     name: new FormControl(''),
     tnk: new FormControl(''),
     profession: new FormControl(''),
-    adjectives: {'key': string, 'value': string}[] = [],
     drive1: new FormControl(''),
     drive2: new FormControl(''),
     drive3: new FormControl(''),
@@ -32,6 +34,19 @@ export class CharacterEditorComponent {
     if(this.adjectives.length < 5) {
       this.adjectives.push({key: this.adjectives.length + '', value: ''});
     }
+  }
+
+  delAdjective(i: number) {
+  }
+
+  addAllegiance() {
+    if(this.allegiances.length < 12) {
+      this.allegiances.push({key: this.allegiances.length + '', value: ''});
+    }
+  }
+
+  addGear() {
+    this.gear.push({key: this.gear.length + '', value: ''});
   }
 }
 
