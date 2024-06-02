adc8647ee8aa672c260ca92827a71e5c66c983f4
colinvh0 <unkempt@gmail.com>
2024-06-01 18:56:03 -0700

    ...

--- src/app/character-editor/character-editor.component.ts
+++ src/app/character-editor/character-editor.component.ts
@@ -1,51 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { Tables } from '../character';
-import { CommonModule } from '@angular/common';
-
-@Component({
-  selector: 'edit-adjectives',
-  standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  template: `
-    <div class="label">Adjectives</div>
-    @if (adjectives.length == 5) {
-      <div>Max 5</div>
-    } @else {
-      <div><button (click)="addAdjective()">Add</button></div>
-    }
-    @for (adj of adjectives; track adj; let i = $index) {
-      <input class="adjectives" id="adj{{i}}" [(ngModel)] = "adjectives[i].value">
-    }
-  `
-})
-class EditAdjectivesComponent {
-  adjectives: {'key': string, 'value': string}[] = [];
-
-  constructor() {
-  }
-
-  addAdjective() {
-    if(this.adjectives.length < 5) {
-      this.adjectives.push({key: this.adjectives.length + '', value: ''});
-    }
-  }
-}
 
 @Component({
   selector: 'app-character-editor',
   standalone: true,
-  imports: [ReactiveFormsModule, EditAdjectivesComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './character-editor.component.html',
   styleUrl: './character-editor.component.less'
 })
 export class CharacterEditorComponent {
-
   formGroup = new FormGroup({
     name: new FormControl(''),
     tnk: new FormControl(''),
     profession: new FormControl(''),
+    adjectives: {'key': string, 'value': string}[] = [],
     drive1: new FormControl(''),
     drive2: new FormControl(''),
     drive3: new FormControl(''),
@@ -56,5 +27,11 @@ export class CharacterEditorComponent {
   });
 
   submit() {}
+
+  addAdjective() {
+    if(this.adjectives.length < 5) {
+      this.adjectives.push({key: this.adjectives.length + '', value: ''});
+    }
+  }
 }
 
